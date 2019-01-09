d77a21d5fe648236dcc58ed10951d64c5f091e42
lordfireriser <arkhan2013@hotmail.com>
2019-01-08 21:59:00 -0500

    updating index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-// Discord.js bot
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "!";
+
 const token = "NTMyMzE0NjMzNDUzNjk5MDcy.DxbdaQ.IbpOQoly7K7LXBGnl3g1OK8GPnI";
 
-client.on("ready", () => {
-  client.user.setActivity("https://git.io/d.js-heroku", { type: "WATCHING" });
+//when a message is sent
+client.on("message", msg => {
+  if (msg.content === "!invite") {
+    msg.channel.send("https://discord.gg/v46vM9Q");
+  }
+
+  msg.react("☺");
 });
 
-client.on("message", msg => {
-  if (!msg.content.startsWith(prefix) || !msg.guild) return;
-
-  const command = msg.content.split(" ")[0].substr(1);
-
-  const args = msg.content
-    .split(" ")
-    .slice(1)
-    .join(" ");
-
-  //bot response
-  if (command === "ping") return msg.channel.send("pong");
-  else if (command === "invite")
-    return msg.channel.send("join here discord.gg/test");
+//bot logged in
+client.on("ready", () => {
+  console.log(console.log("bot has logged on"));
 });
 
 client.login(token);
