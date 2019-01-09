4d588ce4e69d7ee86d8f2acebae22781de3c28c0
lordfireriser <arkhan2013@hotmail.com>
2019-01-08 20:42:08 -0500

    updating the json, fixing the version error

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // Discord.js bot
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "!";
+const token = "NTMyMzE0NjMzNDUzNjk5MDcy.DxbdaQ.IbpOQoly7K7LXBGnl3g1OK8GPnI";
 
-client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+client.on("ready", () => {
+  client.user.setActivity("https://git.io/d.js-heroku", { type: "WATCHING" });
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on("message", msg => {
+  if (!msg.content.startsWith(prefix) || !msg.guild) return;
+
+  const command = msg.content.split(" ")[0].substr(1);
+
+  const args = msg.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+
+  //bot response
+  if (command === "ping") return msg.channel.send("pong");
+  else if (command === "invite")
+    return msg.channel.send("join here discord.gg/test");
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
